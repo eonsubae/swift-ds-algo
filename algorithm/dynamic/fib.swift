f04f68af1b94dfc4599c8e763eeadf106d131da5
eonsubae <eonsubae@naver.com>
2021-08-24 22:58:29 +0900

    Use memoization

--- algorithm/dynamic/fib.swift
+++ algorithm/dynamic/fib.swift
@@ -8,3 +8,20 @@ func fib(_ n: Int) -> Int {
     return fib(n - 1) + fib(n - 2)
   }
 }
+
+// Memoization Def
+// An optimization technique that stores expensive caculated results
+// and returns them when asked for again.
+// Fib using memoization
+var memo: [Int: Int] = []
+
+func mFib(_ n: Int) -> Int {
+  if n == 0 { return 0 } 
+  else if n == 1 { return 1 }
+
+  if let result = memo[n] { return result }
+  
+  memo[n] = mFib(n - 1) + mFib(n - 2)
+
+  return memo[n]!
+}
