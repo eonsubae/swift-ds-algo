db583794863f69b3c9fa893ccba0f56905b79230
eonsubae <eonsubae@naver.com>
2021-08-28 09:08:23 +0900

    leetcode(best time buy and sell stock 1)

--- backjoon/algo-test/algo-test/ViewController.swift
+++ backjoon/algo-test/algo-test/ViewController.swift
@@ -5,28 +5,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
                 
-        print(getRow(3)) // [1,3,3,1]
-        print(getRow(0)) // [1]
-        print(getRow(1)) // [1,1]
+        print(maxProfit([7,1,5,3,6,4])) // 5
+        print(maxProfit([7,6,4,3,1])) // 0
     }
     
     
     
-    func getRow(_ rowIndex: Int) -> [Int] {
-        var memo: [Int: [Int]] = [0: [1], 1: [1, 1]]
-        
-        if rowIndex == 0 || rowIndex == 1 {
-            return memo[rowIndex]!
+    func maxProfit(_ prices: [Int]) -> Int {
+        if prices.isEmpty {
+            return 0
         }
         
-        for i in 2...rowIndex {
-            memo[i] = [Int](repeating: 1, count: i + 1)
-            
-            for j in 1..<memo[i]!.count-1 {
-                memo[i]![j] = memo[i - 1]![j - 1] + memo[i - 1]![j]
+        var min = prices[0]
+        var def = 0
+        
+        for price in prices {
+            if price < min {
+                min = price
+            } else if ((price - min) > def) {
+                def = price - min
             }
         }
         
-        return memo[rowIndex]!
+        return def
     }
 }
