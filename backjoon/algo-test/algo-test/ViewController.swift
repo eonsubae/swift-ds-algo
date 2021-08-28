09934c8ba5de161f2c08c5b063da9381b4c5be3e
eonsubae <eonsubae@naver.com>
2021-08-28 12:27:45 +0900

    leetcode(Get Maximum in Generated Array)

--- backjoon/algo-test/algo-test/ViewController.swift
+++ backjoon/algo-test/algo-test/ViewController.swift
@@ -5,22 +5,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        print(tribonacci(4)) // 4
-        print(tribonacci(25)) // 1389537
+        print(getMaximumGenerated(7)) // 3
+        print(getMaximumGenerated(2)) // 1
+        print(getMaximumGenerated(3)) // 2
     }
     
-    var memo = [Int: Int]()
-    
-    func tribonacci(_ n: Int) -> Int {
+    func getMaximumGenerated(_ n: Int) -> Int {
         if n == 0 { return 0 }
         if n == 1 { return 1 }
-        if n == 2 { return 1 }
         
-        if memo[n] != nil {
-            return memo[n]!
-        } else {
-            memo[n] = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
-            return memo[n]!
+        var generatedArray = [Int](repeating: 0, count: n + 1)
+        generatedArray[0] = 0
+        generatedArray[1] = 1
+        var odd = 2
+        
+        for i in 2...n {
+            if i % 2 == 0 {
+                generatedArray[i] = generatedArray[i / 2]
+            }
+            else {
+                generatedArray[i] = generatedArray[i - odd] + generatedArray[i - odd + 1]
+                odd += 1
+            }
         }
+        
+        return generatedArray.reduce(0) { max($0, $1) }
     }
+    
 }
